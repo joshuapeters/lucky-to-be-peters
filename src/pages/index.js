c86de207c3fd22196b7e0adb734bec26d9648b85
JP <jpeters@andculture.com>
2018-12-15 09:50:46 -0500

    added captcha flag

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,10 @@ const BlogIndex = ({ location }) => {
       <LandingSection imageUrl={imageUrl}>
         <h1>May 25th, 2019</h1>
         <h2>#luckytobepeters</h2>
-        <EmailSignup formText="Enter your email below to receive updates about our big day." />
+        <EmailSignup
+          formText="Enter your email below to receive updates about our big day."
+          hasCaptcha={true}
+        />
       </LandingSection>
     </Layout>
   )
