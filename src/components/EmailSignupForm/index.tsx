ef2ce3b4b630d7aae5c2d80c611895b45d1b8e94
JP <jpeters@andculture.com>
2018-12-16 12:47:13 -0500

    redid netlify form

--- src/components/EmailSignupForm/index.tsx
+++ src/components/EmailSignupForm/index.tsx
@@ -3,7 +3,6 @@ import "./style.scss"
 import { Input, InputType } from 'components/Input/index';
 import { Button } from 'components/Button/index';
 import { NetlifyCaptcha } from 'components/NetlifyCaptcha/index';
-import { navigateTo } from 'gatsby-link';
 
 interface ComponentState {
     email:      string;
@@ -25,21 +24,6 @@ export class EmailSignup extends React.Component<ComponentProps, ComponentState>
         this.onSubscribeClick   = this.onSubscribeClick.bind(this);
     }
 
-    public handleSubmit = e => {
-        e.preventDefault();
-        const form = e.target;
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: this.encode({
-            "form-name": form.getAttribute("name"),
-            ...this.state
-          })
-        })
-          .then(() => navigateTo(form.getAttribute("action")))
-          .catch(error => alert(error));
-      };
-
     public render() {
         const className = "c-email-signup";
         return (
@@ -47,52 +31,40 @@ export class EmailSignup extends React.Component<ComponentProps, ComponentState>
                 <p>{this.props.formText}</p>
                 <form name="contact" method="post" action="/registered" data-netlify="true" data-netlify-honeypot="bot-field">
                     <input type="hidden" name="bot-field" />
-                    { this.getForm(className) }
+                    <div className = {className + "__form"}>
+                        <Input 
+                            type            = { InputType.Text } 
+                            name            = "firstName" 
+                            placeholder     = "First Name" 
+                            required        = { true } 
+                            onChange        = {this.onFieldUpdate}/>
+                        <Input 
+                            type            = { InputType.Text } 
+                            name            = "lastName" 
+                            placeholder     = "Last Name" 
+                            required        = { true } 
+                            onChange        = {this.onFieldUpdate}/>
+                        <Input 
+                            type            = { InputType.Email } 
+                            name            = "email" 
+                            placeholder     = "Email Address" 
+                            cssClassName    = "last" 
+                            required        = { true } 
+                            onChange        = {this.onFieldUpdate}/>
+                        <Button 
+                            type    = "submit"
+                            name    = "email-submit" 
+                            text    = "Subscribe" 
+                            onClick = { this.onSubscribeClick } />
+                        {
+                            this.props.hasCaptcha &&
+                            <NetlifyCaptcha/>
+                        }
+                    </div>
                 </form>
             </div>
         );
     }
-
-    private getForm(className: string) {
-        return (
-            <div className = {className + "__form"}>
-                <Input 
-                    type            = { InputType.Text } 
-                    name            = "firstName" 
-                    placeholder     = "First Name" 
-                    required        = { true } 
-                    onChange        = {this.onFieldUpdate}/>
-                <Input 
-                    type            = { InputType.Text } 
-                    name            = "lastName" 
-                    placeholder     = "Last Name" 
-                    required        = { true } 
-                    onChange        = {this.onFieldUpdate}/>
-                <Input 
-                    type            = { InputType.Email } 
-                    name            = "email" 
-                    placeholder     = "Email Address" 
-                    cssClassName    = "last" 
-                    required        = { true } 
-                    onChange        = {this.onFieldUpdate}/>
-                <Button 
-                    type    = "submit"
-                    name    = "email-submit" 
-                    text    = "Subscribe" 
-                    onClick = { this.onSubscribeClick } />
-                {
-                    this.props.hasCaptcha &&
-                    <NetlifyCaptcha/>
-                }
-            </div>
-        );
-    }
-
-    private encode(data) {
-        return Object.keys(data)
-          .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-          .join("&");
-      }
 
     private onFieldUpdate(fieldId: string, value: any) {
         var newState = {...this.state} as ComponentState;
