a3f53a952ab9881a42ed00542f74ac7fc9659efd
JP <jpeters@andculture.com>
2019-03-31 13:10:47 -0400

    added subtext and time frame to location details

--- src/lib/providers/details/details.ts
+++ src/lib/providers/details/details.ts
@@ -7,6 +7,8 @@ let _locationDetails = [
     {
         location: "Ceremony",
         infoText: "Join us as we tie the knot in Community Menonite Church of Lancaster's beautiful sanctuary.",
+        infoSubtext: "",
+        timeFrame: "11:30am start time",
         imageUrl: cmcl,
         directionsUrl:  "https://goo.gl/maps/SnpV4zJW4QU2",
         websiteUrl:     "https://communitymennonite.org/",
@@ -15,6 +17,8 @@ let _locationDetails = [
     {
         location: "Private Reception",
         infoText: "For select family and friends, join us afterwards for some delicious vegan dishes and mingling at Commonwealth On Queen!",
+        infoSubtext: "",
+        timeFrame: "Following Ceremony",
         imageUrl: commonwealth,
         directionsUrl:  "https://goo.gl/maps/D3mL81hxAHD2",
         websiteUrl:     "https://commonwealthonqueen.com/",
@@ -23,6 +27,8 @@ let _locationDetails = [
     {
         location: "Afterparty",
         infoText: "If we couldn't fit you into the private reception, come grab a slice and a drink with us later at Federal Taphouse in Lancaster!",
+        infoSubtext: "Light refreshments served with a cash bar available.",
+        timeFrame: "6pm - 8pm",
         imageUrl: federaltap,
         directionsUrl:  "https://goo.gl/maps/Ab1DYToosvL2",
         websiteUrl:     "https://www.federaltaphouse.com/lancaster-pa/",
